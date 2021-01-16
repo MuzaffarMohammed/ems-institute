30753f99cb77548a317dfd662da2e5037ff40d62
Muzaffar Mohammed <50576677+MuzaffarMohammed@users.noreply.github.com>
2021-01-16 21:22:23 +0530

    initial changes

---
+++ src/main/java/com/appseonit/ems/institute/EmsInstituteApplication.java
@@ -0,0 +1,13 @@
+package com.appseonit.ems.institute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsInstituteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsInstituteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appseonit/ems/institute/EmsInstituteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appseonit.ems.institute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsInstituteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
